d40a66fdc085710c33607f462bd4d71f939f6a64
Jordan Pettyjohn <pettyjohnwork@gmail.com>
2024-04-29 17:05:53 -0400

    Creating Jordan branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import NavBarComponent from './components/NavBarComponent';
-import Viewer from './pages/Viewer';
+import Viewer from './pages/viewer';
 import Home from './pages/Home';
 import logo from './logo.svg';
 import './App.css';
@@ -10,8 +10,8 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <NavBarComponent />
       <Container>
-        <NavBarComponent />
         <Routes>
           <Route path="/viewer" Component={Viewer} />
           <Route path="/" Component={Home} />
