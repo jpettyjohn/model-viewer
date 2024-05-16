b8ffe78b79bbc91fa3dc6a09076a4b852b5a1e83
Marek Polak <markalexpolak@gmail.com>
2024-05-16 14:44:05 -0400

    added static page handler to server.js

--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,9 @@ app.use(cors());
 //app.use(express.static('wwwroot'));
 app.use(require('./routes/auth.js'));
 app.use(require('./routes/models.js'));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Client/build")));
+  }
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../Client/build/index.html"));
   });
