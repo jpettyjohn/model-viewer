501f02efd7f92bad5c9793826b1d2a360ba06e56
Marek Polak <markalexpolak@gmail.com>
2024-05-16 14:34:45 -0400

    added server code

--- Server/server.js
+++ Server/server.js
@@ -7,4 +7,8 @@ app.use(cors());
 //app.use(express.static('wwwroot'));
 app.use(require('./routes/auth.js'));
 app.use(require('./routes/models.js'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Client/build/index.html"));
+  });
+  
 app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`); });
